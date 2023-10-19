0a65f10ba9974610cf747b6a0892ee518bd42c69
horiavmuntean <horia.muntean@gmail.com>
2023-10-19 19:24:37 +0000

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import "@vaadin/confirm-dialog";
+  import "@vaadin/button";
+  import "@vaadin/date-picker";
+
+  let open = false;
+</script>
+
+<vaadin-confirm-dialog
+  header="Header"
+  cancel
+  on:cancel={(e) => console.log(e)}
+  confirm-text="OK"
+  on:confirm={(e) => console.log(e)}
+  opened={open}
+  on:opened-changed={(e) => (open = e.detail.value)}
+>
+<vaadin-date-picker
+label="Settlement Date"
+style="width: 100%;"
+/>
+</vaadin-confirm-dialog>
+
+<vaadin-button on:click={() => (open = true)}>Open</vaadin-button>
